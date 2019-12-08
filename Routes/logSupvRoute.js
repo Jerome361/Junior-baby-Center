06a0fb1264a1f67e2ef409e070392e6ce1c8fedc
WalterNyeko <nyekowalter69@gmail.com>
2019-12-08 09:00:36 +0300

    implement login functionality

--- Routes/logSupvRoute.js
+++ Routes/logSupvRoute.js
@@ -1,30 +1,41 @@
-const express = require('express');
-const Signup = require('../models/supervisorLog');
-
+const express = require("express");
+const SupervisorLog = require("../models/supervisorLog");
+var bcrypt = require("bcryptjs");
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render('loginSupervisor.html')
+router.get("/", (req, res) => {
+  res.render("loginSupervisor.html");
 });
 
-
-// submits a login page information
-router.post('/', async (req, res) => {
-    try {
-        const user = await SupervisorLog.authenticate(req.body.email, req.body.createPassword);
-        // res.send("hey " + user.firstname + " " + user.lastname)
-
-        req.session.user = user
-
-        res.render('supervisor.html')
-
-    } catch (error) {
-
-        console.log(error);
-
-        res.send("Login Failed")
-        // res.redirect('register')
+const authenticate = async (userEmail, userPassword) => {
+  console.log(userPassword);
+  if (userEmail) {
+    const theUser = [];
+    await SupervisorLog.findOne({ email: userEmail }, (error, response) => {
+      theUser.push(response);
+    });
+    if (theUser.length) {
+      console.log(userPassword, theUser[0].createPassword);
+      return await bcrypt.compareSync(userPassword, theUser[0].createPassword);
+    } else {
+      throw new Error("user is not found");
     }
-})
+  } else {
+    throw new Error("No email provided");
+  }
+};
+// submits a login page information
+router.post("/", async (req, res) => {
+  try {
+    const user = await authenticate(req.body.email, req.body.password);
+    if (user) {
+      res.render("supervisor.html");
+    } else {
+      res.redirect("loginSupervisor.html");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-module.exports = router 
+module.exports = router;
--- models/supervisorLog.js
+++ models/supervisorLog.js
@@ -1,47 +1,35 @@
-const mongoose = require('mongoose')
-//
+const mongoose = require("mongoose");
+
 const supvSchema = new mongoose.Schema({
-    fullName:{
-        type: String,
-        
-    },
-    
-    ninNumber:{
-        type: String,
-        
-    },
-    eidNumber:{
-        type: String,
-        
-    },
-    email:{
-        type: String,
-        
-    },
-    phoneNumber:{
-        type: String,
-        
-    },
-    address:{
-        type: String,
-        
-    },
-    keenName:{
-        type: String,
-        
-    },   
-    keenPhone:{
-        type: String,
-        
-    },
-    createPassword:{
-        type: String,
-        
-    }
-   
-    //cryptographic hashing
-    
+  fullName: {
+    type: String
+  },
+
+  ninNumber: {
+    type: String
+  },
+  eidNumber: {
+    type: String
+  },
+  email: {
+    type: String
+  },
+  phoneNumber: {
+    type: String
+  },
+  address: {
+    type: String
+  },
+  keenName: {
+    type: String
+  },
+  keenPhone: {
+    type: String
+  },
+  createPassword: {
+    type: String
+  }
 
 });
 
-module.exports = mongoose.model('SupervisorLog', supvSchema)
+module.exports = mongoose.model("SupervisorLog", supvSchema);
